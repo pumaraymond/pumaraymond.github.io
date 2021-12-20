accaae7eae27b1fb351bb17efa9d697c6e28d1b6
pumaraymond <91251213+pumaraymond@users.noreply.github.com>
2021-12-21 00:52:20 +0800

    Add files via upload

---
+++ final_html/article.js
@@ -0,0 +1,95 @@
+function Chinese() {
+    document.getElementById("title").innerHTML = "華語歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=8MG--WuNW1Y'>  如果可以</a><br><a href='https://www.youtube.com/watch?v=6O_Zx9St9ik'>  你不屬於我</a><br><a href='https://www.youtube.com/watch?v=G306LT0p9FY'>  不染</a><br><a href='https://www.youtube.com/watch?v=Qx8DWG8x4S4'>  如果能幸福</a><br><a href='https://www.youtube.com/watch?v=4fnEXD-TGyE'>  繭</a><br><a href='https://www.youtube.com/watch?v=dZlT8K_Woo0'>  失重前幸福</a><br><a href='https://www.youtube.com/watch?v=Ryjh-YcO4ag'>  規則就是用來打破的</a><br><a href='https://www.youtube.com/watch?v=_l-Mtr-lCAU'>  時間煮雨</a><br><a href='https://www.youtube.com/watch?v=xoH29SM3e8I'>  知否知否</a><br><a href='https://www.youtube.com/watch?v=A7UU9Z4h56E'>  可樂</a><br><a href='https://www.youtube.com/watch?v=JfnQ8qtcDyQ'>  成全</a><br><a href='https://www.youtube.com/watch?v=2LuW7acW9B8'>  慢冷</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "block";
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("ig").style.display = "none";
+    
+
+}
+function English() {
+    document.getElementById("title").innerHTML = "西洋歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=ZmDBbnmKpqQ'>  Driver's license</a><br><a href='https://www.youtube.com/watch?v=iUNxOzxPEVI'>  I'm not her</a><br><a href='https://www.youtube.com/watch?v=6GUm5g8SG4o'>  Locked away</a><br><a href='https://www.youtube.com/watch?v=foE1mO2yM04'>  I took a pill in Ibiza</a><br><a href='https://www.youtube.com/watch?v=K0ibBPhiaG0'>  Castle on the hill</a><br><a href='https://www.youtube.com/watch?v=BgfcToAjfdc'>  Stole the show</a><br><a href='https://www.youtube.com/watch?v=tt2k8PGm-TI'>  Dusk Till Dawn</a><br><a href='https://www.youtube.com/watch?v=LHCob76kigA'>  7 Years</a><br><a href='https://www.youtube.com/watch?v=eIc4mqyN1Q8'>  Falling</a><br><a href='https://www.youtube.com/watch?v=KBtk5FUeJbk'>  Cradles</a><br><a href='https://www.youtube.com/watch?v=rtOvBOTyX00'>  A Thousand Years</a><br><a href='https://www.youtube.com/watch?v=mw5VIEIvuMI'>  Speechless</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("ig").style.display = "block";
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("profileimg").style.display = "none";
+
+}
+function Japan_Korea() {
+    document.getElementById("title").innerHTML = "日韓歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=clU8c2fpk2s'>  Lemon</a><br><a href='https://www.youtube.com/watch?v=XyzaMpAVm3s'>  This love</a><br><a href='https://www.youtube.com/watch?v=9nCkUgTO5x4'>  灰色と青</a><br><a href='https://www.youtube.com/watch?v=NyXApXudtaU'>  小さな恋のうた</a><br><a href='https://www.youtube.com/watch?v=bGBy80LdkPI'>  あとひとつ</a><br><a href='https://www.youtube.com/watch?v=fokTOFjRfYs'>  時間倒流</a><br><a href='https://www.youtube.com/watch?v=3iR32gC_c7w'>  パプリカ</a><br><a href='https://www.youtube.com/watch?v=fmQRhPSpGdQ'>  夏戀慕</a><br><a href='https://www.youtube.com/watch?v=tDukIfFzX18'>  Maria</a><br><a href='https://www.youtube.com/watch?v=1B9rLGfc9Vo'>  六等星の夜</a><br><a href='https://www.youtube.com/watch?v=hWYM5QEt0Fg'>  시작</a><br><a href='https://www.youtube.com/watch?v=8qK5qHpJXfI'>  まちがいさがし</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "none"
+    document.getElementById("inte").style.display = "block"
+    document.getElementById("ig").style.display = "none";
+
+
+}
+function China() {
+    document.getElementById("title").innerHTML = "中國歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=8ux25IcLr8w/'>  嘉賓</a><br><a href='https://www.youtube.com/watch?v=FaCi8_o8xwQ'>  不在</a><br><a href='https://www.youtube.com/watch?v=0AZ5LnzSDQw'>  間距</a><br><a href='https://www.youtube.com/watch?v=GvKAZ0NJu8Q'>  銀河與星斗</a><br><a href='https://www.youtube.com/watch?v=GvKAZ0NJu8Q'>  千千萬萬</a><br><a href='https://www.youtube.com/watch?v=OqmkthmPBSw'>  下雨天</a><br><a href='https://www.youtube.com/watch?v=6tk9jlKo8y4'>  星辰大海</a><br><a href='https://www.youtube.com/watch?v=oAUmNEUJlJ0'>  囂張</a><br><a href='https://www.youtube.com/watch?v=FxTmGytuAOQ'>  你一定要幸福</a><br><a href='https://www.youtube.com/watch?v=V0XUd8f2pz8'>  飛鳥和禪</a><br><a href='https://www.youtube.com/watch?v=kAP_x0krk7A'>  多想在平凡的生活擁抱你</a><br><a href='https://www.youtube.com/watch?v=HWrDNKJ8TwU'>  雨下的瞬間</a><br><a href='https://www.youtube.com/watch?v=WZiSPQBY3d8</a><br>"
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "none"
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("ig").style.display = "none";
+    
+}
