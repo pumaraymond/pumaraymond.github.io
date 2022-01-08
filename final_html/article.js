5b2d98fb5f8494a3d7bb313a4d043670178fc82f
pumaraymond <91251213+pumaraymond@users.noreply.github.com>
2022-01-08 15:28:18 +0800

    Add files via upload

---
+++ final_html/article.js
@@ -0,0 +1,95 @@
+function Chinese() {
+    document.getElementById("title").innerHTML = "華語歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=8MG--WuNW1Y'target='_blank'target='_blank'>  如果可以</a><br><a href='https://www.youtube.com/watch?v=6O_Zx9St9ik'target='_blank'target='_blank'>  你不屬於我</a><br><a href='https://www.youtube.com/watch?v=G306LT0p9FY'target='_blank'target='_blank'>  不染</a><br><a href='https://www.youtube.com/watch?v=Qx8DWG8x4S4'target='_blank'target='_blank'>  如果能幸福</a><br><a href='https://www.youtube.com/watch?v=4fnEXD-TGyE'target='_blank'target='_blank'>  繭</a><br><a href='https://www.youtube.com/watch?v=dZlT8K_Woo0'target='_blank'target='_blank'>  失重前幸福</a><br><a href='https://www.youtube.com/watch?v=Ryjh-YcO4ag'target='_blank'target='_blank'>  規則就是用來打破的</a><br><a href='https://www.youtube.com/watch?v=_l-Mtr-lCAU'target='_blank'target='_blank'>  時間煮雨</a><br><a href='https://www.youtube.com/watch?v=xoH29SM3e8I'target='_blank'target='_blank'>  知否知否</a><br><a href='https://www.youtube.com/watch?v=A7UU9Z4h56E'target='_blank'target='_blank'>  可樂</a><br><a href='https://www.youtube.com/watch?v=JfnQ8qtcDyQ'target='_blank'target='_blank'>  成全</a><br><a href='https://www.youtube.com/watch?v=2LuW7acW9B8'target='_blank'target='_blank'>  慢冷</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "block";
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("ig").style.display = "none";
+    
+
+}
+function English() {
+    document.getElementById("title").innerHTML = "西洋歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=ZmDBbnmKpqQ'target='_blank'target='_blank'>  Driver's license</a><br><a href='https://www.youtube.com/watch?v=iUNxOzxPEVI'target='_blank'target='_blank'>  I'm not her</a><br><a href='https://www.youtube.com/watch?v=6GUm5g8SG4o'target='_blank'>  Locked away</a><br><a href='https://www.youtube.com/watch?v=foE1mO2yM04'target='_blank'>  I took a pill in Ibiza</a><br><a href='https://www.youtube.com/watch?v=K0ibBPhiaG0'target='_blank'>  Castle on the hill</a><br><a href='https://www.youtube.com/watch?v=BgfcToAjfdc'target='_blank'>  Stole the show</a><br><a href='https://www.youtube.com/watch?v=tt2k8PGm-TI'target='_blank'>  Dusk Till Dawn</a><br><a href='https://www.youtube.com/watch?v=LHCob76kigA'target='_blank'>  7 Years</a><br><a href='https://www.youtube.com/watch?v=eIc4mqyN1Q8'target='_blank'>  Falling</a><br><a href='https://www.youtube.com/watch?v=KBtk5FUeJbk'target='_blank'>  Cradles</a><br><a href='https://www.youtube.com/watch?v=rtOvBOTyX00'target='_blank'>  A Thousand Years</a><br><a href='https://www.youtube.com/watch?v=mw5VIEIvuMI'target='_blank'>  Speechless</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("ig").style.display = "block";
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("profileimg").style.display = "none";
+
+}
+function Japan_Korea() {
+    document.getElementById("title").innerHTML = "日韓歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=clU8c2fpk2s'target='_blank'>  Lemon</a><br><a href='https://www.youtube.com/watch?v=XyzaMpAVm3s'target='_blank'>  This love</a><br><a href='https://www.youtube.com/watch?v=9nCkUgTO5x4'target='_blank'>  灰色と青</a><br><a href='https://www.youtube.com/watch?v=NyXApXudtaU'target='_blank'>  小さな恋のうた</a><br><a href='https://www.youtube.com/watch?v=bGBy80LdkPI'target='_blank'>  あとひとつ</a><br><a href='https://www.youtube.com/watch?v=fokTOFjRfYs'target='_blank'>  時間倒流</a><br><a href='https://www.youtube.com/watch?v=3iR32gC_c7w'target='_blank'>  パプリカ</a><br><a href='https://www.youtube.com/watch?v=fmQRhPSpGdQ'target='_blank'>  夏戀慕</a><br><a href='https://www.youtube.com/watch?v=tDukIfFzX18'target='_blank'>  Maria</a><br><a href='https://www.youtube.com/watch?v=1B9rLGfc9Vo'target='_blank'>  六等星の夜</a><br><a href='https://www.youtube.com/watch?v=hWYM5QEt0Fg'target='_blank'>  시작</a><br><a href='https://www.youtube.com/watch?v=8qK5qHpJXfI'target='_blank'>  まちがいさがし</a><br>";
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "none"
+    document.getElementById("inte").style.display = "block"
+    document.getElementById("ig").style.display = "none";
+
+
+}
+function China() {
+    document.getElementById("title").innerHTML = "中國歌曲\n";
+    document.getElementById("title").style.position = "absolute";
+    document.getElementById("title").style.top = "8vw";
+    document.getElementById("title").style.left = "10vw";
+    document.getElementById("title").style.fontSize = "40px";
+    document.getElementById("title").style.fontFamily = "'微軟正黑體', monospace";
+    document.getElementById("title").style.fontWeight = "100";
+    document.getElementById("title").style.display = "block";
+    
+    document.getElementById("article").style.display = "block";
+    
+    document.getElementById("article").innerHTML = "<a href='https://www.youtube.com/watch?v=8ux25IcLr8w/'target='_blank'>  嘉賓</a><br><a href='https://www.youtube.com/watch?v=FaCi8_o8xwQ'target='_blank'>  不在</a><br><a href='https://www.youtube.com/watch?v=0AZ5LnzSDQw'target='_blank'>  間距</a><br><a href='https://www.youtube.com/watch?v=GvKAZ0NJu8Q'target='_blank'>  銀河與星斗</a><br><a href='https://www.youtube.com/watch?v=GvKAZ0NJu8Q'target='_blank'>  千千萬萬</a><br><a href='https://www.youtube.com/watch?v=OqmkthmPBSw'target='_blank'>  下雨天</a><br><a href='https://www.youtube.com/watch?v=6tk9jlKo8y4'target='_blank'>  星辰大海</a><br><a href='https://www.youtube.com/watch?v=oAUmNEUJlJ0'target='_blank'>  囂張</a><br><a href='https://www.youtube.com/watch?v=FxTmGytuAOQ'target='_blank'>  你一定要幸福</a><br><a href='https://www.youtube.com/watch?v=V0XUd8f2pz8'target='_blank'>  飛鳥和禪</a><br><a href='https://www.youtube.com/watch?v=kAP_x0krk7A'target='_blank'>  多想在平凡的生活擁抱你</a><br><a href='https://www.youtube.com/watch?v=HWrDNKJ8TwU'target='_blank'>  雨下的瞬間</a><br><a href='https://www.youtube.com/watch?v=WZiSPQBY3d8</a><br>"
+    document.getElementById("article").style.position = "absolute";
+    document.getElementById("article").style.top = "13vw";
+    document.getElementById("article").style.left = "12vw";
+    document.getElementById("article").style.fontSize = "30px";
+    document.getElementById("article").style.fontFamily = "'微軟正黑體', monospace";
+
+    document.getElementById("profileimg").style.display = "none"
+    document.getElementById("inte").style.display = "none"
+    document.getElementById("ig").style.display = "none";
+    
+}
